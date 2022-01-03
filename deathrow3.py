5ec2112522385a944a17ce8c9ec49f0d1358b935
S.B <lunarwolf01@gmail.com>
2022-01-02 20:23:41 -0800

    2cbf733ec453e0302a3976b3d9e9f3c1314aa4df actually fix it

--- deathrow3.py
+++ deathrow3.py
@@ -8,7 +8,7 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from os import replace
 import random
-import bs4 as bs4
+from bs4 import BeautifulSoup
 import time
 import pytumblr
 from imgurpython import ImgurClient
